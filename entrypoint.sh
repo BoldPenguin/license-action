c167f61a51d504cd001a7ad03279483ea375f01a
Jared Szechy <jared@boldpenguin.com>
2021-06-18 15:33:40 -0400

    Add some debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,8 @@ curl -fsS \
   -o dependency_decisions.yml \
   -L https://api.github.com/repos/BoldPenguin/licensed-config/contents/dependency_decisions.yml
 
+pwd
+cat .bundle/config
+ls vendor/bundle/ruby/*/gems
+
 license_finder --decisions-file=dependency_decisions.yml
