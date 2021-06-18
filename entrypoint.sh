7aeeece0e6974513c7bfb92c84613f67bd4654d7
Jared Szechy <jared@boldpenguin.com>
2021-06-18 16:00:20 -0400

    Use correct config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
 curl -fsS \
   -H "Authorization: token $BP_GH_TOKEN" \
@@ -6,10 +6,17 @@ curl -fsS \
   -o dependency_decisions.yml \
   -L https://api.github.com/repos/BoldPenguin/licensed-config/contents/dependency_decisions.yml
 
+pwd
+cat .bundle/config
+bundle config
+
+unset BUNDLE_APP_CONFIG
+echo "Updating bundle path"
 # The absolute path will be different when running in Docker
-bundle config --local BUNDLE_PATH $PWD/vendor/bundle
+bundle config --local path $PWD/vendor/bundle
 
 cat .bundle/config
+bundle config
 ls vendor/bundle/ruby/*/gems
 
 license_finder --decisions-file=dependency_decisions.yml
