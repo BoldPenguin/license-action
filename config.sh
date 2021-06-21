32671c2e025d4e410ac55998c3b95a0faa7548b5
Jared Szechy <jared@boldpenguin.com>
2021-06-21 13:42:40 -0400

    Update repo name

--- config.sh
+++ config.sh
@@ -6,4 +6,4 @@ curl -fsS \
   -H "Authorization: token $BP_GH_TOKEN" \
   -H 'Accept: application/vnd.github.v3.raw' \
   -o doc/dependency_decisions.yml \
-  -L https://api.github.com/repos/BoldPenguin/licensed-config/contents/dependency_decisions.yml
+  -L https://api.github.com/repos/BoldPenguin/license-config/contents/dependency_decisions.yml
