dc07ed7c274e3c4db6eb2c3d51e980e661aaa0ba
Jared Szechy <jared@boldpenguin.com>
2021-06-21 13:00:04 -0400

    Use default file location

--- config.sh
+++ config.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
+mkdir -p doc
+
 curl -fsS \
   -H "Authorization: token $BP_GH_TOKEN" \
   -H 'Accept: application/vnd.github.v3.raw' \
-  -o dependency_decisions.yml \
+  -o doc/dependency_decisions.yml \
   -L https://api.github.com/repos/BoldPenguin/licensed-config/contents/dependency_decisions.yml
--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ if [ -f Gemfile.lock ]; then
   gem install bundler -v 2.1.4
 fi
 
-$CMD_PREFIX license_finder --decisions-file=dependency_decisions.yml
+$CMD_PREFIX license_finder
