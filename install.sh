c883214c2af46f5701e2a3e3c28e96550216dfcd
Isaac Post <isaac.post@boldpenguin.com>
2022-05-03 17:52:50 -0400

    debug

--- install.sh
+++ install.sh
@@ -3,14 +3,10 @@
 if [ -f Gemfile.lock ]; then
   bundle config unset deployment
   bundle add license_finder --group=development
-  pwd
-  echo "================================================================================"
-  ls
-  echo "================================================================================"
-  ls ..
-  echo "================================================================================"
-  ls vendor
-  echo "================================================================================"
+  echo "pwd: $(pwd)"
+  echo "ls: $(ls)"
+  echo "ls ..: $(ls ..)"
+  echo "ls -a vendor: $(ls -a vendor)"
 else
   gem install license_finder
 fi
