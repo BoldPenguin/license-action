41562de7c388a539a4ddd194ce6b58665cd1b12b
Isaac Post <isaac.post@boldpenguin.com>
2022-05-03 18:02:09 -0400

    debug

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -ex
 
 if [ -f Gemfile.lock ]; then
+  ls -a vendor/bundle/ruby/3.1.0/gems
   bundle config unset deployment
   bundle add license_finder --group=development
   # echo "pwd: $(pwd)"
   # echo "ls: $(ls)"
   # echo "ls ..: $(ls ..)"
-  ls -a vendor
+  ls -a vendor/bundle/ruby/3.1.0/gems
   file vendor/bundle
 else
   gem install license_finder
