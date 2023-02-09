afeaf9c2b87e2663389005238b42d8eb5f861576
Jeremy Nicklas <jeremywnicklas@gmail.com>
2023-02-09 17:44:09 -0500

    fix: BP-1 | Take 2 at fixing bundler issues

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 if [ -f Gemfile.lock ]; then
   bundle config unset deployment
   bundle add license_finder --group=development
+  bundle lock --add-platform x86_64-linux
 else
   gem install license_finder
 fi
