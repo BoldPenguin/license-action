8989845d1caa3ddbc42c4d53228ec4227b5c42ce
Jeremy Nicklas <jeremywnicklas@gmail.com>
2023-02-09 15:57:07 -0500

    fix: BP-1 | Failing to run with Ruby 3.2.1 (#4)

--- run.sh
+++ run.sh
@@ -4,10 +4,7 @@ CMD_PREFIX=""
 ARGS=""
 
 if [ -f Gemfile.lock ]; then
-  CMD_PREFIX="bundle _2.1.4_ exec"
-  # Bundler has an issue, need to run using an old version
-  # https://github.com/pivotal/LicenseFinder/issues/828
-  gem install bundler -v 2.1.4
+  CMD_PREFIX="bundle exec"
 fi
 
 if ! [ -z "$RECURSIVE" ]; then
